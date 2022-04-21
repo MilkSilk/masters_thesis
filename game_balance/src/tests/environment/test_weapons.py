cc3bca8715d9f2f1a8bf39bdec692d94337839d5
MilkSilk <jac.jankow@gmail.com>
2022-04-21 16:24:04 +0200

    Comments to weapon test

--- game_balance/src/tests/environment/test_weapons.py
+++ game_balance/src/tests/environment/test_weapons.py
@@ -5,7 +5,11 @@ weapons = define_weapons()
 
 @pytest.mark.parametrize("weapon", weapons)
 def test_weapon_dealing_damage(weapon):
+    # make sure we don't run out of ammo
     weapon.ammo_loaded = 10_000
+
+    # Check obvious full dmg up to effective range 
+    # and 0 damage when at or over 2*effective range
     assert weapon.deal_damage(0) == weapon.damage
     assert weapon.deal_damage(weapon.effective_range/2) == weapon.damage
     assert weapon.deal_damage(weapon.effective_range/4) == weapon.damage
@@ -15,6 +19,8 @@ def test_weapon_dealing_damage(weapon):
     assert weapon.deal_damage(2*weapon.effective_range+1) == 0
     assert weapon.deal_damage(3*weapon.effective_range) == 0 
 
+    # Check if damage between 1 effective and 2 effective ranges are below a line and in descending order
+    # Maybe there is a better way to test a parabola, but this is good enough for now
     dmgs_in_parabola = []
     for i in range(weapon.effective_range, weapon.effective_range*2, 3):
         new_dmg = weapon.deal_damage(i)
