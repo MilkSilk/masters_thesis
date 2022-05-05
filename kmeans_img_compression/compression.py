1257912a25dd9af51911923b79a6d854e4fa24b6
MilkSilk <jac.jankow@gmail.com>
2022-05-05 16:31:15 +0200

    First huffman encoding changes

--- kmeans_img_compression/compression.py
+++ kmeans_img_compression/compression.py
@@ -2,29 +2,6 @@ from PIL import Image
 from sklearn.cluster import KMeans
 from dahuffman import HuffmanCodec
 
-codec = HuffmanCodec.from_frequencies({'e': 100, 'n':20, 'x':1, 'i': 40, 'q':3})
-encoded = codec.encode('exeneeeexniqneieini')
-encoded
-# b'\x86|%\x13i@'
-len(encoded)
-# 6
-codec.decode(encoded)
-# 'exeneeeexniqneieini'
-codec.print_code_table()
-# bits  code       (value)  symbol
-#    5  00000      (    0)  _EOF
-#    1  1          (    1)  'e'
-#    2  01         (    1)  'i'
-#    3  001        (    1)  'n'
-#    4  0001       (    1)  'q'
-#    5  00001      (    1)  'x'
-
-codec = HuffmanCodec.from_data('hello world how are you doing today foo bar lorem ipsum')
-codec.encode('do lo er ad od')
-# b'^O\x1a\xc4S\xab\x80'
-len(_)
-# 7
-
 im_source = "kmeans_img_compression/images/doggo.jpg"
 
 def compress_image(im_source, k):
@@ -52,17 +29,46 @@ def compress_image(im_source, k):
             new_image.putpixel((col, row), tuple(centroids[assigned_centroids[col*new_image.height+row]]))
     return new_image, raw_image
 
+def huffman_enocode(im_data):
+    codec = HuffmanCodec.from_data(str(im_data))
+    print(str(im_data[:100]))
+    codec_table = codec.get_code_table()
+    print(codec_table)
+    codec.print_code_table()
+    encoded_im = codec.encode(str(im_data))
+    return encoded_im, codec_table
+
 def save_compressed_im(raw_im_data, n_colors):
     with open(f"kmeans_img_compression/images/{n_colors}_doggo_raw.txt", mode="w") as doggo_im_raw:
         doggo_im_raw.write(str(raw_im_data[0])+"\n")
         
         doggo_im_raw.writelines('\t'.join(str(j) for j in i) + '\n' for i in raw_im_data[1:])
+    with open(f"kmeans_img_compression/images/{n_colors}_huffman_naive_doggo_raw.txt", mode="wb") as doggo_im_raw:
+        # Saves both pixel values (centroids) and pixels huffman encoded to a file
+        encoded_im, codec_table = huffman_enocode(raw_im_data)
+        codec_table_bytes = str.encode(codec_table)
+
+        doggo_im_raw.write(codec_table_bytes)
+        doggo_im_raw.write(encoded_im)
+
+    with open(f"kmeans_img_compression/images/{n_colors}_huffman_doggo_raw.txt", mode="wb") as doggo_im_raw:
+        # Saves both pixel values (centroids) raw and pixels huffman encoded to a file
+        doggo_im_raw.write(str.encoderaw_im_data[0])
+        encoded_im, codec_table = huffman_enocode(raw_im_data[1:])
+        codec_table_bytes = str.encode(codec_table)
+        
+        doggo_im_raw.write(codec_table_bytes)
+        doggo_im_raw.write(encoded_im)
 
 
-color_nums = [2, 5, 10, 20, 50, 100]
+# color_nums = [2, 5, 10, 20, 50, 100]
+
+
+color_nums = [2, 5]
 
 for color_num in color_nums:
     new_image, raw_image = compress_image(im_source, color_num)
     save_compressed_im(raw_image, color_num)
 
     new_image.save(f"kmeans_img_compression/images/{color_num}_doggo.jpg")
+
--- kmeans_img_compression/size_checks.py
+++ kmeans_img_compression/size_checks.py
@@ -2,8 +2,14 @@ import os
 
 color_nums = [2, 5, 10, 20, 50, 100]
 
-print("Basic image in jpg size:", os.stat("kmeans_img_compression/images/doggo.jpg").st_size)
+# color_nums = [2, 5]
+
+
+print("Basic image in jpg:", os.stat("kmeans_img_compression/images/doggo.jpg").st_size%1000, "KB")
 
 for color_num in color_nums:
-    print(f"Raw image size with {color_num} colors size:", os.stat(f"kmeans_img_compression/images/{color_num}_doggo_raw.txt").st_size)
-    print(f"Image with {color_num} colors size:", os.stat(f"kmeans_img_compression/images/{color_num}_doggo.jpg").st_size)
+    print(f"Raw image {color_num} colors:", os.stat(f"kmeans_img_compression/images/{color_num}_doggo_raw.txt").st_size%1000, "KB")
+    print(f"Huffman encoded all with {color_num} colors:", os.stat(f"kmeans_img_compression/images/{color_num}_huffman_naive_doggo_raw.txt").st_size%1000, "KB")
+    print(f"Huffman encoded just image data with {color_num} colors:", os.stat(f"kmeans_img_compression/images/{color_num}_huffman_doggo_raw.txt").st_size%1000, "KB")
+    print(f"Image with {color_num} colors:", os.stat(f"kmeans_img_compression/images/{color_num}_doggo.jpg").st_size%1000, "KB")
+    print()
